ae90ee9ab75d5d8a0c8d5996bdd1a3bb5a5640fd
Minsu <jminsu084@gmail.com>
2022-03-28 13:21:16 +0900

    ADD : Controller와 View 페이지를 연결 및 템플릿 변수를 사용하기 위해 Model 추가

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,5 +1,16 @@
 package com.example.firstproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class FirstController {
+
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "Minsu");
+        return "greetings"; // templates/greetings.mustache를 찾아서 브라우저로 전송
+    }
 }
